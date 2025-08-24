1aa6bf42ea68c56ab185908e89e735098ffb2b4f
Ozcon99 <ozcon09@gmail.com>
2025-08-24 07:34:10 +0700

    Update Kahr-Ddos.py

--- Kahr-Ddos.py
+++ Kahr-Ddos.py
@@ -70,7 +70,7 @@ def main():
     bar.finish()
 
     while True:
-        print(("\033[0;37;40m Sending Keep-Alive Headers to {}".format(len(socket_list))))
+        print(("\033[0;37;40m [÷] Kahr builds an attack {}".format(len(socket_list))))
 
         for s in socket_list:
             try:
